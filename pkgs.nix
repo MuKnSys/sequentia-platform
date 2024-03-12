c204d3bc2d67ba4879218ed8031a6f6b98ddf418
JBetz <joebetz91@gmail.com>
2024-03-12 18:39:26 +0100

    Add nix shell flag to build sequentia locally or from github repo

--- pkgs.nix
+++ pkgs.nix
@@ -1,10 +1,21 @@
 let
   systemPkgs = import <nixpkgs> {};
-  nixpkgs-repo = systemPkgs.fetchFromGitHub {
+  nixpkgs-source = systemPkgs.fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
     rev = "0deaf4d5d224fac3cb2ae9c92a4e349c277be982";
     sha256 = "sha256-uERpVxRrCUB7ySkGb3NtDmzEkPDn23VfkCtT2hJZty8=";
   };
-  pkgs = import nixpkgs-repo {};
+  sequentia-source = pkgs.fetchFromGitHub {
+    owner = "MuKnIO";
+    repo = "SEQ-Core-Elements";
+    rev = "ac958ffe6b0963059ca2b21df5d9e7150474b07a";
+    sha256 = "sha256-G0EA/FYrPWHjLGKWW3+OVzCoThtfqoFt/9gFgaOI8Aw="; 
+  };
+  config = {
+    packageOverrides = pkgs: rec {
+      sequentia = pkgs.callPackage sequentia-source {};
+    };
+  };
+  pkgs = import nixpkgs-source { inherit config; };
 in pkgs
--- shell.nix
+++ shell.nix
@@ -1,16 +1,19 @@
+{ withLocalSequentia ? false 
+}:
 let pkgs = import ./pkgs.nix;
-    gerbilInputs = [
+    sequentia = if withLocalSequentia then pkgs.callPackage ../SEQ-Core-Elements/default.nix {} else pkgs.sequentia;
+    gerbilPackages = [ 
         pkgs.gerbilPackages-unstable.gerbil-utils
-        pkgs.gerbilPackages-unstable.gerbil-poo 
-    ];
+        pkgs.gerbilPackages-unstable.gerbil-poo
+    ]; 
 in pkgs.mkShell {
-    buildInputs = [
+    buildInputs = gerbilPackages ++ [
         pkgs.gerbil-unstable
-        pkgs.bitcoin
-    ] ++ gerbilInputs;
+        sequentia
+    ];
 
     shellHook = ''
-        export GERBIL_LOADPATH=${pkgs.gerbil-support.gerbilLoadPath (["$out"] ++ gerbilInputs)}
+        export GERBIL_LOADPATH=${pkgs.gerbil-support.gerbilLoadPath (["$out"] ++ gerbilPackages)}
         export GERBIL_PATH=$PWD/.build
         export GERBIL_BUILD_CORES=$NIX_BUILD_CORES
     '';
