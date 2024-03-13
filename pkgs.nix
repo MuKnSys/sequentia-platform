6f4017d60f466a040c1c6019e6b10953ae79c629
Francois-Rene Rideau <fare@tunes.org>
2024-03-12 23:55:22 -0400

    Fix bad case

--- pkgs.nix
+++ pkgs.nix
@@ -6,11 +6,11 @@ let
     rev = "0deaf4d5d224fac3cb2ae9c92a4e349c277be982";
     sha256 = "sha256-uERpVxRrCUB7ySkGb3NtDmzEkPDn23VfkCtT2hJZty8=";
   };
-  sequentia-source = systempkgs.fetchFromGitHub {
+  sequentia-source = systemPkgs.fetchFromGitHub {
     owner = "MuKnIO";
     repo = "SEQ-Core-Elements";
     rev = "ac958ffe6b0963059ca2b21df5d9e7150474b07a";
-    sha256 = "sha256-G0EA/FYrPWHjLGKWW3+OVzCoThtfqoFt/9gFgaOI8Aw="; 
+    sha256 = "sha256-G0EA/FYrPWHjLGKWW3+OVzCoThtfqoFt/9gFgaOI8Aw=";
   };
   config = {
     packageOverrides = pkgs: rec {
