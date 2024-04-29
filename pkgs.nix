a1fa9ad90a0686465760fba717e0a65baf7a2ef9
François-René Rideau <fare@tunes.org>
2024-04-29 10:45:34 -0400

    Update nixpkgs for MuKn

--- pkgs.nix
+++ pkgs.nix
@@ -3,8 +3,8 @@ let
   nixpkgs-source = systemPkgs.fetchFromGitHub {
     owner = "MuKnIO";
     repo = "nixpkgs";
-    rev = "98803955f43860497cdb785343448d6c454aaede";
-    sha256 = "sha256-+UoKXNKJPxXc6Tb8znIKC+wptAhwg6bqP3RPQqICNC0=";
+    rev = "7e0ac2b84a76241917f8458d4050fd3316f9588c";
+    sha256 = "sha256-T5TCY6wJkmez4LZJAjR6TVOlT5XPryTD18ydUJ9x9rU=";
   };
   sequentia-source = systemPkgs.fetchFromGitHub {
     owner = "MuKnIO";
