1442fb035a779abe0b7d5c6c77cdd540bdce4031
François-René Rideau <fare@tunes.org>
2024-05-12 15:25:57 -0400

    Temporary fix for nix-shell

--- shell.nix
+++ shell.nix
@@ -23,9 +23,17 @@ in pkgs.mkShell {
         pkgs.clang-tools
     ] ++ pkgs.lib.optionals (!withLocalSequentia) [sequentia];
 
+    # Until gerbil-support includes static compilation objects,
+    # let's not use gerbilLoadPath and instead copy the sources into the writable GERBIL_PATH.
     shellHook = ''
-        export GERBIL_LOADPATH=${pkgs.gerbil-support.gerbilLoadPath (["$out"] ++ gerbilPackages)}
-        export GERBIL_PATH=$PWD/.build
+        export GERBIL_PATH=$PWD/.gerbil
+        mkdir -p $GERBIL_PATH
+        chmod -R u+w .gerbil
+        #export GERBIL_LOADPATH=${pkgs.gerbil-support.gerbilLoadPath (["$out"] ++ gerbilPackages)}
+        for i in ${pkgs.lib.concatStringsSep " " gerbilPackages} ; do
+          cp -af $i/gerbil/lib $GERBIL_PATH/
+          chmod -R u+w .gerbil
+        done
         export GERBIL_BUILD_CORES=$NIX_BUILD_CORES
         ${if withLocalSequentia then
            let sce = ../SEQ-Core-Elements/src ; in
